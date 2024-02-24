a659f0de7cd776cc07cd46221744f481be1fa98a
Mohit Varma <mv501049@gmail.com>
2024-02-24 19:44:01 +0530

    Update ArrayPractice.java

--- src/com/mohit/ArrayPractice.java
+++ src/com/mohit/ArrayPractice.java
@@ -6,6 +6,7 @@ class Main {
         
         println(Arrays.toString(strs));
         int[] array1 = new int[]{3,4,5,2,1};//getRandomIntArray(10);
+        
         println(Arrays.toString(array1));
         Arrays.sort(array1); // dual-pivot quicksort alogrithm
         println(Arrays.toString(array1));
