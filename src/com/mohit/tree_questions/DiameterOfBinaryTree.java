ae317fa452f0ff00fd0db736d49579d926a350b8
Mohit Varma <mv501049@gmail.com>
2024-03-17 23:57:02 +0530

    Update DiameterOfBinaryTree.java

--- src/com/mohit/tree_questions/DiameterOfBinaryTree.java
+++ src/com/mohit/tree_questions/DiameterOfBinaryTree.java
@@ -6,6 +6,7 @@ public class DiameterOfBinaryTree {
     
     private int height(TreeNode node) {
         if (node == null) return 0;
+        
         return 1 + Math.max(height(node.left), height(node.right));
     }
     public int diameter(TreeNode node) {
